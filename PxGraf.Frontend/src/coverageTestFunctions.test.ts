095b38d268107d2323bdd2bdf87c321b26941ed8
Sakari Malkki <sakari.malkki@stat.fi>
2024-06-03 10:23:23 +0300

    Remove frontend coverage test functions and their tests

--- PxGraf.Frontend/src/coverageTestFunctions.test.ts
+++
@@ -1,8 +0,0 @@
-import { coveredTestFunction } from './coverageTestFunctions';
-
-describe('coverage tests', () => {
-    it('should return right number', () => {
-        const result = coveredTestFunction(5);
-        expect(result).toEqual(10);
-    });
-});
--- PxGraf.Frontend/src/coverageTestFunctions.ts
+++
@@ -1,9 +0,0 @@
-export function coveredTestFunction(input: number) {
-    const output: number = input * 2;
-    return output;
-}
-
-export function uncoveredTestFunction(input: number) {
-    const output: number = input * 3;
-    return output;
-}
