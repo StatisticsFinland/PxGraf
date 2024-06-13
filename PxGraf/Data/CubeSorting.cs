39d45a9685099397461b869e52488e2e76175846
Sakari Malkki <sakari.malkki@stat.fi>
2024-06-13 13:03:33 +0300

    Remove bunch of deprecated code and tests

--- PxGraf/Data/CubeSorting.cs
+++ PxGraf/Data/CubeSorting.cs
@@ -60,24 +60,6 @@ namespace PxGraf.Data
             };
         }
 
-        /// <summary>
-        /// One dimensional, ascending or descending sorting
-        /// </summary>
-        public static DataCube SortOneDimensionalCharts(DataCube cube, string sorting)
-        {
-            return cube.GetTransform(GetSortedMapForOneDimensionalCharts(cube, sorting));
-        }
-
-        /// <summary>
-        /// Sum or variable value based, descending sorting
-        /// </summary>
-        public static DataCube SortMultidimHorizontalBarChart(VisualizationType visualization, DataCube cube, string sorting, bool pivotRequested)
-        {
-            return cube.GetTransform(GetSortedMapForMultidimHorizontalBarChart(visualization, cube, sorting, pivotRequested));
-        }
-
-        #region PRIVATES
-
         #region SORTING_OPTIONS
 
         // Not sortable at the moment
@@ -134,83 +116,6 @@ namespace PxGraf.Data
 
         #endregion
 
-        #region UTILITY_GETS
-
-        /// <summary>
-        /// One dimensional, ascending or descending sorting
-        /// </summary>
-        private static CubeMap GetSortedMapForOneDimensionalCharts(DataCube cube, string sorting)
-        {
-            if (sorting == DESCENDING || sorting == ASCENDING)
-            {
-                var multiselects = cube.Meta.GetMultivalueVariables();
-                if (multiselects.Count == 0) return cube.Meta.BuildMap(); // Sorting one value does nothing.
-
-                // If thre are selectable variables and therefore many multiselects, we want to select the last one because it will be in the view
-                var multiselectVariable = multiselects[multiselects.Count - 1];
-                if (sorting == DESCENDING)
-                {
-                    return cube.Meta.BuildMap().CloneWithEditedVariable(multiselectVariable.Code, DescendingSumSorting(multiselectVariable, cube));
-                }
-                else
-                {
-                    return cube.Meta.BuildMap().CloneWithEditedVariable(multiselectVariable.Code, SumSorting(multiselectVariable, cube));
-                }
-            }
-            else
-            {
-                return cube.Meta.BuildMap();
-            }
-        }
-
-        /// <summary>
-        /// Sum or variable value based, descending sorting
-        /// </summary>
-        private static CubeMap GetSortedMapForMultidimHorizontalBarChart(VisualizationType visualization, DataCube cube, string sorting, bool pivotRequested)
-        {
-            if (sorting == NO_SORTING || string.IsNullOrEmpty(sorting)) return cube.Meta.BuildMap();
-
-            var sortingVariable = GetSortingVariable(visualization, cube.Meta, pivotRequested);
-            if (sorting == SUM)
-            {
-                return cube.Meta.BuildMap().CloneWithEditedVariable(sortingVariable.Code, DescendingSumSorting(sortingVariable, cube));
-            }
-            else if (sorting == REVERSED)
-            {
-                var sortingValue = GetSortingVariable(visualization, cube.Meta, pivotRequested);
-                var reversedValues = sortingValue.IncludedValues.Reverse().ToList();
-                return cube.Meta.BuildMap().CloneWithEditedVariable(sortingValue.Code, reversedValues);
-            }
-            else // sorted based on particular variable value
-            {
-                //OBS: sortingVariable and sortingOptionsVariable are not the same thing!
-                // sortingOptionsVariable = pick one value and sort the values of the sortingVariable besed on that value.
-                // sortingVariable = these value will be sorted
-                var sortingOptionsVariable = GetSortingOptionsVariable(visualization, cube.Meta, pivotRequested);
-                var focusValue = sortingOptionsVariable.IncludedValues.First(vv => vv.Code == sorting);
-                var sortingValueOrder = VariableValueSorting(focusValue, sortingOptionsVariable, sortingVariable, cube);
-                var transformMap = cube.Meta.BuildMap().CloneWithEditedVariable(sortingVariable.Code, sortingValueOrder);
-
-                // when the cube is sorted based on particualr variable value, it is moved to be the first value of that variable.
-                var sortingOptionAsFirst = new List<IReadOnlyVariableValue> { focusValue };
-                sortingOptionAsFirst.AddRange(sortingOptionsVariable.IncludedValues.ToList().FindAll(v => v != focusValue));
-
-                return transformMap.CloneWithEditedVariable(sortingOptionsVariable.Code, sortingOptionAsFirst);
-            }
-        }
-
-        private static IReadOnlyVariable GetSortingVariable(VisualizationType visualization, IReadOnlyCubeMeta meta, bool manualPivotRequested)
-        {
-            var multiselects = meta.GetMultivalueVariables();
-            return GetPivot(visualization, meta, manualPivotRequested) ? multiselects[0] : multiselects[1];
-        }
-
-        private static IReadOnlyVariable GetSortingOptionsVariable(VisualizationType visualization, IReadOnlyCubeMeta meta, bool pivotRequested)
-        {
-            var multiselects = meta.GetMultivalueVariables();
-            return GetPivot(visualization, meta, pivotRequested) ? multiselects[1] : multiselects[0];
-        }
-
         private static List<SortingOption> GetVariableSortingOptions(IReadOnlyVariable variable)
         {
             List<SortingOption> options = [];
@@ -222,21 +127,12 @@ namespace PxGraf.Data
             return options;
         }
 
-        private static bool GetPivot(VisualizationType visualization, IReadOnlyCubeMeta meta, bool manualPivotRequest)
-        {
-            bool autoPivot = AutoPivotRules.GetAutoPivot(visualization, meta);
-            bool manualPivot = ManualPivotRules.GetManualPivotability(visualization, meta) && manualPivotRequest;
-            return autoPivot ^ manualPivot;
-        }
-
         private static bool GetPivot(VisualizationType visualization, IReadOnlyCubeMeta meta, VisualizationSettingsRequest request)
         {
             bool autoPivot = AutoPivotRules.GetAutoPivot(visualization, meta, request.Query);
             bool manualPivot = ManualPivotRules.GetManualPivotability(visualization, meta, request.Query) && request.PivotRequested;
             return autoPivot ^ manualPivot;
         }
-
-        #endregion
 
         #region SORTING_TYPES
 
@@ -299,43 +195,6 @@ namespace PxGraf.Data
 
             return new SortingOption(REVERSED, translations);
         }
-
-        #endregion
-
-        #region SORTING_FUNCTIONS
-
-        private static List<IReadOnlyVariableValue> DescendingSumSorting(IReadOnlyVariable sortingVariable, DataCube cube)
-        {
-            return [.. sortingVariable.IncludedValues
-                .OrderByDescending(v =>
-                   {
-                       var map = cube.Meta.BuildMap().CollapseOneVariableInMap(v, sortingVariable);
-                       return cube.GetTransform(map).CalculateCubeSum();
-                   }
-                )];
-        }
-
-        private static List<IReadOnlyVariableValue> SumSorting(IReadOnlyVariable sortingVariable, DataCube data)
-        {
-            return [.. sortingVariable.IncludedValues
-                .OrderBy(v =>
-                    {
-                        var map = data.Meta.BuildMap().CollapseOneVariableInMap(v, sortingVariable);
-                        return data.GetTransform(map).CalculateCubeSum();
-                    }
-                )];
-        }
-
-        private static List<IReadOnlyVariableValue> VariableValueSorting(
-            IReadOnlyVariableValue sortingBaseVariableValue,
-            IReadOnlyVariable sortingBaseVariable,
-            IReadOnlyVariable sortingVariable,
-            DataCube data)
-        {
-            return DescendingSumSorting(sortingVariable, data.GetTransform(data.Meta.BuildMap().CollapseOneVariableInMap(sortingBaseVariableValue, sortingBaseVariable)));
-        }
-
-        #endregion
 
         #endregion
     }
