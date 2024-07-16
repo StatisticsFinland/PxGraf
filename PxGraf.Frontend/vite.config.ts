66061c70bedbe94e938fb596688c419f7c33e540
Sakari Malkki <sakari.malkki@stat.fi>
2024-07-16 12:59:01 +0300

    Clean up by building on top of a fresh vite project

--- PxGraf.Frontend/vite.config.ts
+++ PxGraf.Frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,8 +16,7 @@ export default defineConfig({
             'hooks': '/src/hooks',
             'api': '/src/api',
             'utils': '/src/utils',
-            'images': '/src/images',
-            'dummy': '/src/dummy',
+            'images': '/src/images'
         }
     },
     server: {
