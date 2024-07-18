7b199f5b5b57055eeae02823555a62209f2aed30
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-07-18 10:09:25 +0300

    Added interface for data source and initial implementation for the local filesystem database.

---
+++ PxGraf/Datasource/DatabaseConnection/DatabaseItems/DatabaseGroupHeader.cs
@@ -0,0 +1,10 @@
+﻿using Px.Utils.Language;
+
+namespace PxGraf.Datasource.DatabaseConnection.DatabaseItems
+{
+    public class DatabaseGroupHeader(string code, MultilanguageString name)
+    {
+        public string Code { get; } = code;
+        public MultilanguageString Name { get; } = name;
+    }
+}
