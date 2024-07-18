98be998b01a1e2b53744ed8e409879ffbf2d1268
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-07-18 10:09:25 +0300

    Added interface for data source and initial implementation for the local filesystem database.

---
+++ PxGraf/Datasource/DatabaseConnection/CachedLocalFilesystemDatabase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Px.Utils.Models.Metadata;
+using PxGraf.Models.Queries;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PxGraf.Datasource.DatabaseConnection
+{
+    public class CachedLocalFilesystemDatabase(IMemoryCache cache, ILocalFilesystemDatabaseConnection dbConnection)
+    {
+        private readonly IMemoryCache _cache = cache;
+        private readonly ILocalFilesystemDatabaseConnection _dbConnection = dbConnection;
+
+        private TimeSpan _slidingExpiration = TimeSpan.FromHours(1);
+        private TimeSpan _absoluteExpiration = TimeSpan.FromHours(12);
+        private const string FILE_HASH_SEED = "pNlNfoAkxtzCDkNaFqx9";
+
+        private sealed class MetadataCacheHousing(DateTime writeTime, IReadOnlyMatrixMetadata metadata)
+        {
+            public DateTime CacheTime { get; } = DateTime.Now;
+            public DateTime WriteTime { get; } = writeTime;
+            public IReadOnlyMatrixMetadata Metadata { get; } = metadata;
+        }
+
+        public IReadOnlyMatrixMetadata GetMatrixMetadataCached(PxTableReference fileReference)
+        {
+            string hash = GetCacheKeyFromFileReference(fileReference);
+            DateTime lastWriteTime = _dbConnection.CheckLastWritetime(fileReference);
+            if (_cache.TryGetValue(hash, out MetadataCacheHousing item) && item.WriteTime == lastWriteTime)
+            {
+                return item.Metadata;
+            }
+            else
+            {
+                IReadOnlyMatrixMetadata newMeta = _dbConnection.ReadMatrixMetadata(fileReference);
+                MetadataCacheHousing newCacheItem = new(lastWriteTime, newMeta);
+                MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(_slidingExpiration)
+                    .SetAbsoluteExpiration(_absoluteExpiration);
+                _cache.Set(hash, newCacheItem, cacheEntryOptions);
+                return newMeta;
+            }
+        }
+
+        private static string GetCacheKeyFromFileReference(PxTableReference fileReference)
+        {
+            StringBuilder builder = new();
+
+            fileReference.Hierarchy.ForEach(x => builder.Append(x));
+            builder.Append(fileReference.Name);
+            builder.Append(FILE_HASH_SEED);
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(builder.ToString());
+            byte[] hashBytes = MD5.HashData(inputBytes);
+
+            return BitConverter.ToString(hashBytes);
+        }
+    }
+}
---
+++ PxGraf/Datasource/DatabaseConnection/LocalFilesystemDatabase.cs
@@ -0,0 +1,138 @@
+﻿#nullable enable
+using Px.Utils.Language;
+using Px.Utils.ModelBuilders;
+using Px.Utils.Models;
+using Px.Utils.Models.Data.DataValue;
+using Px.Utils.Models.Metadata;
+using Px.Utils.PxFile.Data;
+using Px.Utils.PxFile.Metadata;
+using PxGraf.Datasource.DatabaseConnection.DatabaseItems;
+using PxGraf.Models.Queries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PxGraf.Datasource.DatabaseConnection
+{
+    public class LocalFilesystemDatabase(LocalFilesystemDatabaseConfig config, Func<PxTableReference, string> referenceToPathConverter) : IDatasource
+    {
+        private readonly Func<PxTableReference, string> _referenceToPath = referenceToPathConverter;
+
+        /// <inheritdoc/>
+        public DatabaseGroupContents GetGroupContents(IReadOnlyList<string> groupHierarcy)
+        {
+            List<DatabaseGroupHeader> headers = [];
+            List<PxTableReference> tables = [];
+
+            string path = Path.Combine(config.DatabaseRootPath, string.Join(Path.PathSeparator, groupHierarcy));
+            foreach (string directory in Directory.EnumerateDirectories(path))
+            {
+                string code = new DirectoryInfo(directory).Name;
+                MultilanguageString alias = GetGroupName(directory);
+                headers.Add(new DatabaseGroupHeader(code, alias));
+            }
+
+            const string PX_FILE_FILTER = "*.px";
+            foreach (string pxFile in Directory.EnumerateFiles(path, PX_FILE_FILTER))
+            {
+                tables.Add(new PxTableReference(pxFile.Remove(0, config.DatabaseRootPath.Length)));
+            }
+
+            return new DatabaseGroupContents(headers, tables);
+        }
+
+        /// <inheritdoc/>
+        public async Task<DatabaseGroupContents> GetGroupContentsAsync(IReadOnlyList<string> groupHierarcy)
+        {
+            return await Task.Factory.StartNew(() => GetGroupContents(groupHierarcy));
+        }
+
+        /// <inheritdoc/>
+        public DateTime GetLastWriteTime(PxTableReference tableReference)
+        {
+            string path = _referenceToPath(tableReference);
+            return Directory.GetLastWriteTime(path);
+        }
+
+        /// <inheritdoc/> 
+        public async Task<DateTime> GetLastWriteTimeAsync(PxTableReference tableReference)
+        {
+            string path = _referenceToPath(tableReference);
+            return await Task.Factory.StartNew(() => Directory.GetLastWriteTime(path));
+        }
+
+        /// <inheritdoc/> 
+        public Matrix<DecimalDataValue> GetMatrix(PxTableReference tableReference, IReadOnlyMatrixMetadata meta, IMatrixMap completeMap)
+        {
+            string path = _referenceToPath(tableReference);
+            DataIndexer indexer = new(completeMap, meta);
+            Matrix<DecimalDataValue> output = new(meta, new DecimalDataValue[indexer.DataLength]);
+            using Stream fileStream = File.OpenRead(path);
+            PxFileStreamDataReader dataReader = new(fileStream);
+            dataReader.ReadDecimalDataValues(output.Data, 0, indexer);
+            return output;
+        }
+
+        /// <inheritdoc/> 
+        public async Task<Matrix<DecimalDataValue>> GetMatrixAsync(
+            PxTableReference tableReference,
+            IReadOnlyMatrixMetadata meta,
+            IMatrixMap completeMap,
+            CancellationToken? cancellationToken = null
+            )
+        {
+            string path = _referenceToPath(tableReference);
+            DataIndexer indexer = new(completeMap, meta);
+            Matrix<DecimalDataValue> output = new(meta, new DecimalDataValue[indexer.DataLength]);
+            using Stream fileStream = File.OpenRead(path);
+            PxFileStreamDataReader dataReader = new(fileStream);
+            if (cancellationToken is null) await dataReader.ReadDecimalDataValuesAsync(output.Data, 0, indexer); 
+            else await dataReader.ReadDecimalDataValuesAsync(output.Data, 0, indexer, cancellationToken.Value);
+            return output;
+        }
+
+        /// <inheritdoc/> 
+        public IReadOnlyMatrixMetadata GetMatrixMetadata(PxTableReference tableReference)
+        {
+            string path = _referenceToPath(tableReference);
+            using Stream readStream = File.OpenRead(path);
+            IEnumerable<KeyValuePair<string, string>> entries = PxFileMetadataReader.ReadMetadata(readStream, config.Encoding);
+            MatrixMetadataBuilder builder = new();
+            return builder.Build(entries);
+        }
+
+        /// <inheritdoc/> 
+        public async Task<IReadOnlyMatrixMetadata> GetMatrixMetadataAsync(PxTableReference tableReference)
+        {
+            string path = _referenceToPath(tableReference);
+            using Stream readStream = File.OpenRead(path);
+            IAsyncEnumerable<KeyValuePair<string, string>> entries = PxFileMetadataReader.ReadMetadataAsync(readStream, config.Encoding);
+            MatrixMetadataBuilder builder = new();
+            return await builder.BuildAsync(entries);
+        }
+
+        /// <summary>
+        /// Assumes the alias files to be named Alias_[lang].txt where [lang] is the language code of the alias.
+        /// </summary>
+        /// <param name="path">Path to the folder containing the allias files.</param>
+        /// <returns>Multilanguage string of the alias</returns>
+        private MultilanguageString GetGroupName(string path)
+        {
+            Dictionary<string, string> translatedNames = [];
+            const string ALIAS_FILE_FILTER = "*.txt";
+            IEnumerable<string> aliasFiles = Directory.GetFiles(path, ALIAS_FILE_FILTER)
+                .Where(p => Path.GetFileName(p).StartsWith("alias", StringComparison.OrdinalIgnoreCase));
+            foreach (string aliasFile in aliasFiles)
+            {
+                string lang = new([.. Path.GetFileName(aliasFile).Skip(6).TakeWhile(c => c != '.')]);
+                string alias = File.ReadAllText(aliasFile, config.Encoding);
+                translatedNames.Add(lang, alias.Trim());
+            }
+            return new MultilanguageString(translatedNames);
+        }
+    }
+}
+#nullable disable
