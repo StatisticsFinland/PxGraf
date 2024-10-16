4216405cbde45cbb9dc2ea1cc245d945e39d3c0e
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-10-16 13:12:22 +0300

    The backend fixes for the table listing errors.

--- PxGraf/Models/Responses/DatabaseItems/DatabaseTable.cs
+++ PxGraf/Models/Responses/DatabaseItems/DatabaseTable.cs
@@ -1,16 +1,44 @@
-﻿using Px.Utils.Language;
-using System;
+﻿#nullable enable
+using Px.Utils.Language;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System;
 
 namespace PxGraf.Models.Responses.DatabaseItems
 {
-    public class DatabaseTable(string code, MultilanguageString name, DateTime lastUpdated, List<string> languages)
+    public class DatabaseTable
     {
-        public string Code { get; } = code;
-        public MultilanguageString Name { get; } = name;
+        public string Code { get; private set; }
 
-        public DateTime LastUpdated { get; } = lastUpdated;
+        public MultilanguageString? Name { get; private set; }
 
-        public List<string> Languages { get; } = languages;
+        public DateTime? LastUpdated { get; private set; }
+
+        public List<string> Languages { get; private set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? Error { get; private set; } = null;
+
+        public DatabaseTable(string code, MultilanguageString name, DateTime lastUpdated, List<string> languages)
+        {
+            Code = code;
+            Name = name;
+            LastUpdated = lastUpdated;
+            Languages = languages;
+        }
+
+        private DatabaseTable(string code, MultilanguageString? name, List<string> languages)
+        {
+            Code = code;
+            Name = name;
+            Languages = languages;
+            Error = true;
+        }
+
+        public static DatabaseTable FromError(string code, MultilanguageString? name, List<string> languages)
+        {
+            return new DatabaseTable(code, name, languages);
+        }
     }
 }
+#nullable disable
