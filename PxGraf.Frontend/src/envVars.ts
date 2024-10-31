8705b9ddd2c12958957777215b606dbe2809847f
Sakari Malkki <sakari.malkki@stat.fi>
2024-10-31 08:49:12 +0200

    Excludes env vars file from frontend coverage

--- PxGraf.Frontend/src/envVars.ts
+++ PxGraf.Frontend/src/envVars.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 export const PxGrafUrl: string = import.meta.env.VITE_PXGRAF_URL;
 export const PublicUrl: string = import.meta.env.PUBLIC_URL;
 export const BasePath: string = import.meta.env.VITE_BASE_PATH || '';
