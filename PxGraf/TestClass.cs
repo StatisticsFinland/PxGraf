c3ae904a07723bfe428558772de6b7b8352a1236
Sakari Malkki <sakari.malkki@stat.fi>
2024-12-04 15:54:31 +0200

    Add test class and tests with partial coverage

---
+++ PxGraf/TestClass.cs
@@ -0,0 +1,15 @@
+﻿namespace PxGraf
+{
+    public class TestClass
+    {
+        public int TestMethodA(int input)
+        {
+            return input + 1;
+        }
+
+        public int TestMethodB(int input)
+        {
+            return input + 2;
+        }
+    }
+}
