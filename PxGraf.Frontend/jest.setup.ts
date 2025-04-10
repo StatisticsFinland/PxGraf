fe932b7f240e531953a4fe7040cc524742005721
Jose Saarimaa <jose.saarimaa@stat.fi>
2025-04-10 08:17:55 +0300

    Fixed issues with missing definition of CSS.supports

--- PxGraf.Frontend/jest.setup.ts
+++ PxGraf.Frontend/jest.setup.ts
@@ -1,4 +1,7 @@
 import { TextEncoder, TextDecoder } from 'util';
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+
+if (!window.CSS) window.CSS = {} as any;
+if (!window.CSS.supports) window.CSS.supports = () => true;
