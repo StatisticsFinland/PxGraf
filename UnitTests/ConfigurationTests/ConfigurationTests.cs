b3cf561e456507881c302d1bd7125c98cf2cdba0
Sakari Malkki <sakari.malkki@stat.fi>
2025-06-05 13:22:52 +0300

    More tests

---
+++ UnitTests/ConfigurationTests/ConfigurationTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using PxGraf.Exceptions;
+using PxGraf.Language;
+using PxGraf.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTests.Fixtures;
+
+namespace UnitTests.ConfigurationTests
+{
+    public class ConfigurationTests
+    {
+
+        [OneTimeSetUp]
+        public void DoSetup()
+        {
+            Localization.Load(TranslationFixture.DefaultLanguage, TranslationFixture.Translations);
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_Success()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(TestInMemoryConfiguration.Get())
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current, Is.Not.Null);
+            Assert.That(Configuration.Current.PxWebUrl, Is.EqualTo("http://pxwebtesturl:12345/"));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithoutConfiguration_ThrowsException()
+        {
+            Assert.Throws<NullReferenceException>(() => Configuration.Load(null));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithoutLocalDatabase_SuccessWithDefaultValues()
+        {
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"pxwebUrl", "http://pxwebtesturl:12345/"}
+                    })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current, Is.Not.Null);
+            Assert.That(Configuration.Current.PxWebUrl, Is.EqualTo("http://pxwebtesturl:12345/"));
+            Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig.Enabled, Is.False);
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithoutPxWebUrlWithLocalDatabaseConfig_Success()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"LocalFilesystemDatabaseConfig:Enabled", "true"},
+                        {"LocalFilesystemDatabaseConfig:Encoding", "latin1"},
+                        {"LocalFilesystemDatabaseConfig:DatabasePath", "path/to/database"}
+                    })
+                .Build();
+            Configuration.Load(configuration);
+            Assert.That(Configuration.Current, Is.Not.Null);
+            Assert.That(Configuration.Current.PxWebUrl, Is.Null);
+            Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig.Enabled, Is.True);
+            Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig.Encoding, Is.EqualTo(Encoding.Latin1));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithoutPxWebUrlAndLocalDatabase_ThrowsException()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"pxwebUrl", null},
+                    })
+                .Build();
+            Assert.Throws<InvalidConfigurationException>(() => Configuration.Load(configuration));
+        }
+    }
+}
