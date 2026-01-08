c089b1a33eb9ef86321e7953e77292d1fba793f9
Sakari Malkki <sakari.malkki@stat.fi>
2026-01-08 10:27:14 +0200

    Tests

--- UnitTests/ConfigurationTests/ConfigurationTests.cs
+++ UnitTests/ConfigurationTests/ConfigurationTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using PxGraf.Exceptions;
 using PxGraf.Language;
@@ -106,6 +107,99 @@ namespace UnitTests.ConfigurationTests
             Assert.That(Configuration.Current, Is.Not.Null);
             Assert.That(Configuration.Current.PxWebUrl, Is.EqualTo("http://pxwebtesturl:12345/"));
             Assert.That(Configuration.Current.LocalFilesystemDatabaseConfig, Is.Null);
+        }
+
+        [Test]
+        public void AppSettings_WhenApplicationInsightsConfigurationNotProvided_ShouldLoadWithDisabledApplicationInsights()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"LocalFilesystemDatabaseConfig:Enabled", "true"},
+                        {"LocalFilesystemDatabaseConfig:Encoding", "latin1"},
+                        {"LocalFilesystemDatabaseConfig:DatabaseRootPath", "path/to/database"}
+                    })
+                .Build();
+
+            // Act
+            Configuration.Load(configuration);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(Configuration.Current.ApplicationInsights.IsEnabled, Is.False);
+                Assert.That(Configuration.Current.ApplicationInsights.ConnectionString, Is.Null);
+                Assert.That(Configuration.Current.ApplicationInsights.MinimumLevel, Is.EqualTo(LogLevel.Information));
+                Assert.That(Configuration.Current.ApplicationInsights.EnableAdaptiveSampling, Is.False);
+            });
+        }
+
+        [Test]
+        public void AppSettings_WhenApplicationInsightsConfigurationProvided_ShouldLoadApplicationInsightsSettings()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"pxwebUrl", "http://pxwebtesturl:12345/"},
+                        {"ApplicationInsights:ConnectionString", "InstrumentationKey=test-key;IngestionEndpoint=https://test.com"},
+                        {"ApplicationInsights:MinimumLevel", "Debug"},
+                        {"ApplicationInsights:EnableAdaptiveSampling", "true"}
+                    })
+                .Build();
+
+            // Act
+            Configuration.Load(configuration);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(Configuration.Current.ApplicationInsights.IsEnabled, Is.True);
+                Assert.That(Configuration.Current.ApplicationInsights.ConnectionString, Is.EqualTo("InstrumentationKey=test-key;IngestionEndpoint=https://test.com"));
+                Assert.That(Configuration.Current.ApplicationInsights.MinimumLevel, Is.EqualTo(LogLevel.Debug));
+                Assert.That(Configuration.Current.ApplicationInsights.EnableAdaptiveSampling, Is.True);
+            });
+        }
+
+        [Test]
+        public void AppSettings_WhenApplicationInsightsEnvironmentVariableSet_ShouldPrioritizeEnvironmentVariable()
+        {
+            // Arrange
+            const string envVarName = "APPLICATIONINSIGHTS_CONNECTION_STRING";
+            const string envConnectionString = "InstrumentationKey=env-key;IngestionEndpoint=https://test.com";
+
+            Environment.SetEnvironmentVariable(envVarName, envConnectionString);
+
+            try
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(
+                        new Dictionary<string, string>
+                        {
+                            {"pxwebUrl", "http://pxwebtesturl:12345/"},
+                            {"ApplicationInsights:ConnectionString", "InstrumentationKey=config-key;IngestionEndpoint=https://test.com"},
+                            {"ApplicationInsights:MinimumLevel", "Warning"}
+                        })
+                    .Build();
+
+                // Act
+                Configuration.Load(configuration);
+
+                // Assert - Environment variable should take priority over config
+                Assert.Multiple(() =>
+                {
+                    Assert.That(Configuration.Current.ApplicationInsights.IsEnabled, Is.True);
+                    Assert.That(Configuration.Current.ApplicationInsights.ConnectionString, Is.EqualTo(envConnectionString));
+                    Assert.That(Configuration.Current.ApplicationInsights.MinimumLevel, Is.EqualTo(LogLevel.Warning)); // Other settings from config should still work
+                });
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable(envVarName, null);
+            }
         }
     }
 }
