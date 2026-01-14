c1d4acd4c4edaa4e658ac9b0efda8f17e1a83f97
Sakari Malkki <sakari.malkki@stat.fi>
2026-01-14 14:00:00 +0200

    ﻿Improved configuration test coverage

--- UnitTests/ConfigurationTests/ConfigurationTests.cs
+++ UnitTests/ConfigurationTests/ConfigurationTests.cs
@@ -189,6 +189,200 @@ namespace UnitTests.ConfigurationTests
         }
 
         [Test]
+        public void ConfigurationLoadTest_WithLocalQueryStorageConfig_Success()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"LocalQueryStorageConfig:Enabled", "true"},
+                    {"LocalQueryStorageConfig:SavedQueryDirectory", "/path/to/queries"},
+                    {"LocalQueryStorageConfig:ArchiveFileDirectory", "/path/to/archives"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.LocalQueryStorageConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.Enabled, Is.True);
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.SavedQueryDirectory, Is.EqualTo("/path/to/queries"));
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.ArchiveFileDirectory, Is.EqualTo("/path/to/archives"));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithBlobQueryStorageConfig_Success()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"BlobQueryStorageConfig:Enabled", "true"},
+                    {"BlobQueryStorageConfig:StorageAccountName", "teststorage"},
+                    {"BlobQueryStorageConfig:ContainerName", "testcontainer"},
+                    {"BlobQueryStorageConfig:SavedQueryPath", "custom-queries"},
+                    {"BlobQueryStorageConfig:ArchiveFilePath", "custom-archives"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.BlobQueryStorageConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.Enabled, Is.True);
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.StorageAccountName, Is.EqualTo("teststorage"));
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.ContainerName, Is.EqualTo("testcontainer"));
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.SavedQueryPath, Is.EqualTo("custom-queries"));
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.ArchiveFilePath, Is.EqualTo("custom-archives"));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithBlobQueryStorageConfig_EmptyPathsReturnNull()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"BlobQueryStorageConfig:Enabled", "true"},
+                    {"BlobQueryStorageConfig:StorageAccountName", "teststorage"},
+                    {"BlobQueryStorageConfig:ContainerName", "testcontainer"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.BlobQueryStorageConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.SavedQueryPath, Is.Null);
+            Assert.That(Configuration.Current.BlobQueryStorageConfig.ArchiveFilePath, Is.Null);
+        }
+
+        [Test]
+        public void SetQueryDirectoryFields_WithBlobQueryStorage_SetsLegacyFields()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"BlobQueryStorageConfig:Enabled", "true"},
+                    {"BlobQueryStorageConfig:StorageAccountName", "teststorage"},
+                    {"BlobQueryStorageConfig:ContainerName", "testcontainer" },
+                    {"BlobQueryStorageConfig:SavedQueryPath", "custom-saved"},
+                    {"BlobQueryStorageConfig:ArchiveFilePath", "custom-archive"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.SavedQueryDirectory, Is.EqualTo("custom-saved"));
+            Assert.That(Configuration.Current.ArchiveFileDirectory, Is.EqualTo("custom-archive"));
+        }
+
+        [Test]
+        public void SetQueryDirectoryFields_WithLocalQueryStorage_SetsLegacyFields()
+        {
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"LocalQueryStorageConfig:Enabled", "true"},
+                    {"LocalQueryStorageConfig:SavedQueryDirectory", "/local/saved"},
+                    {"LocalQueryStorageConfig:ArchiveFileDirectory", "/local/archive"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.SavedQueryDirectory, Is.EqualTo("/local/saved"));
+            Assert.That(Configuration.Current.ArchiveFileDirectory, Is.EqualTo("/local/archive"));
+        }
+
+        [Test]
+        public void SetQueryDirectoryFields_WithLegacyStorage_SetsLegacyFields()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                 new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"savedQueryDirectory", "/legacy/saved"},
+                    {"archiveFileDirectory", "/legacy/archive"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.SavedQueryDirectory, Is.EqualTo("/legacy/saved"));
+            Assert.That(Configuration.Current.ArchiveFileDirectory, Is.EqualTo("/legacy/archive"));
+        }
+
+        [Test]
+        public void SetQueryDirectoryFields_WithLegacyFallback_FromLocalQueryStorageConfig()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"savedQueryDirectory", "/legacy/saved"},
+                    {"archiveFileDirectory", "/legacy/archive"}
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            // Should create LocalQueryStorageConfig from legacy settings
+            Assert.That(Configuration.Current.LocalQueryStorageConfig, Is.Not.Null);
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.Enabled, Is.True);
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.SavedQueryDirectory, Is.EqualTo("/legacy/saved"));
+            Assert.That(Configuration.Current.LocalQueryStorageConfig.ArchiveFileDirectory, Is.EqualTo("/legacy/archive"));
+            // And set legacy fields
+            Assert.That(Configuration.Current.SavedQueryDirectory, Is.EqualTo("/legacy/saved"));
+            Assert.That(Configuration.Current.ArchiveFileDirectory, Is.EqualTo("/legacy/archive"));
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithMissingBlobQueryStorageFields_ReturnsNull()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"BlobQueryStorageConfig:Enabled", "true"},
+                    {"BlobQueryStorageConfig:StorageAccountName", "teststorage"}
+                    // ContainerName missing
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.BlobQueryStorageConfig, Is.Null);
+        }
+
+        [Test]
+        public void ConfigurationLoadTest_WithMissingLocalQueryStorageFields_ReturnsNull()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {"pxwebUrl", "http://test.com"},
+                    {"LocalQueryStorageConfig:Enabled", "true"},
+                    {"LocalQueryStorageConfig:SavedQueryDirectory", "/path/to/queries"}
+                      // ArchiveFileDirectory missing
+                })
+                .Build();
+
+            Configuration.Load(configuration);
+
+            Assert.That(Configuration.Current.LocalQueryStorageConfig, Is.Null);
+        }
+
+        [Test]
         public void AppSettings_WhenApplicationInsightsConfigurationNotProvided_ShouldLoadWithDisabledApplicationInsights()
         {
             // Arrange
