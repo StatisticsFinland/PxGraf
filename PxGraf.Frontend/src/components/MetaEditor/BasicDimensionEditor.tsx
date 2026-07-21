3c3107882047d44e4be5bcc4242fbbef28b17efb
Jose Saarimaa <jose.saarimaa@stat.fi>
2026-07-21 12:20:24 +0300

    refactor: update BasicDimensionEditor to use Grid layout for better responsiveness

--- PxGraf.Frontend/src/components/MetaEditor/BasicDimensionEditor.tsx
+++ PxGraf.Frontend/src/components/MetaEditor/BasicDimensionEditor.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Stack } from '@mui/material';
+import { Grid } from '@mui/material';
 import { UiLanguageContext } from 'contexts/uiLanguageContext';
 import { EditorField } from './Editorfield';
 import styled from 'styled-components';
 import { IDimension } from 'types/cubeMeta';
 import { QueryContext } from '../../contexts/queryContext';
 
-const EditorFieldWrapper = styled(Stack)`
-  padding: 0px;
+const EditorFieldWrapper = styled.div`
+  padding-top: 0.5rem;
+  padding-right: 1rem;
+  max-height: 50vh;
+  overflow-y: auto;
 `;
 
 interface IBasicDimensionEditor {
@@ -47,18 +50,25 @@ export const BasicDimensionEditor: React.FC<IBasicDimensionEditor> = ({ dimensio
     };
 
     return (
-        <EditorFieldWrapper spacing={2}>
-            {dimension.values.map(value => {
-                return (
-                    <EditorField
-                        label={t("editMetadata.valueName") + ": " + value.name[uiContentLanguage]}
-                        key={value.code}
-                        defaultValue={value.name[language]}
-                        editValue={dimensionEdits?.valueEdits?.[value.code]?.nameEdit?.[language]}
-                        onChange={newValue => handleChange(newValue, value.code)}
-                    />
-                );
-            })}
+        <EditorFieldWrapper>
+            <Grid container spacing={3}>
+                {dimension.values.map(value => {
+                    return (
+                        <Grid
+                            key={value.code}
+                            size={{ xs: 12, md: dimension.values.length > 5 ? 6 : 12 }}
+                        >
+                            <EditorField
+                                label={t("editMetadata.valueName") + ": " + value.name[uiContentLanguage]}
+                                defaultValue={value.name[language]}
+                                editValue={dimensionEdits?.valueEdits?.[value.code]?.nameEdit?.[language]}
+                                onChange={newValue => handleChange(newValue, value.code)}
+                                style={{ width: '100%' }}
+                            />
+                        </Grid>
+                    );
+                })}
+            </Grid>
         </EditorFieldWrapper>
     );
 }
